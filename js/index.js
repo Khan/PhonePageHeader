3fe5cb03037af94000012ab3f087d47f3c3d33a7
Nefaur Khandker <nefaurk@gmail.com>
2015-07-09 11:32:23 -0700

    - Initial commit

---
+++ js/index.js
@@ -0,0 +1,388 @@
+var pageTitles = [
+  "Arithmetic",
+  "Addition and subtraction",
+  "Biology",
+  "Cells",
+  "Prehistoric art in Europe and West",
+  "Neolithic art",
+  "LeBron asks"
+];
+var pageIcons = [
+  "page-icons/Math-icons-01.png",
+  "page-icons/Math-icons-02.png",
+  "page-icons/Math-icons-03.png",
+  "page-icons/Math-icons-04.png",
+  "page-icons/Math-icons-05.png",
+  "page-icons/Math-icons-06.png",
+  "page-icons/Math-icons-07.png",
+  "page-icons/Math-icons-08.png",
+  "page-icons/Math-icons-09.png",
+  "page-icons/Math-icons-10.png",
+  "page-icons/Math-icons-11.png",
+  "page-icons/Math-icons-12.png",
+  "page-icons/Math-icons-13.png",
+  "page-icons/Math-icons-14.png",
+  "page-icons/Math-icons-15.png",
+  "page-icons/Math-icons-16.png",
+  "page-icons/Math-icons-17.png",
+  "page-icons/Math-icons-18.png",
+  "page-icons/Math-icons-19.png",
+  "page-icons/Math-icons-20.png",
+  "page-icons/Math-icons-21.png",
+  "page-icons/Math-icons-22.png",
+  "page-icons/Math-icons-23.png",
+  "page-icons/Math-icons-24.png",
+  "page-icons/Math-icons-25.png",
+  "page-icons/Math-icons-26.png",
+  "page-icons/Math-icons-27.png",
+  "page-icons/Math-icons-28.png",
+  "page-icons/Math-icons-29.png",
+  "page-icons/Math-icons-30.png",
+  "page-icons/Math-icons-31.png",
+  "page-icons/Math-icons-32.png",
+  "page-icons/Math-icons-33.png",
+  "page-icons/Math-icons-34.png",
+  "page-icons/Math-icons-35.png",
+  "page-icons/Math-icons-36.png",
+  "page-icons/Math-icons-37.png",
+  "page-icons/Math-icons-38.png",
+  "page-icons/Math-icons-39.png",
+  "page-icons/Math-icons-40.png",
+  "page-icons/Math-icons-41.png",
+  "page-icons/Math-icons-42.png",
+  "page-icons/Math-icons-43.png",
+  "page-icons/Math-icons-44.png",
+  "page-icons/Math-icons-45.png",
+  "page-icons/Math-icons-46.png",
+  "page-icons/Math-icons-47.png",
+  "page-icons/Math-icons-48.png",
+  "page-icons/Math-icons-49.png",
+  "page-icons/Math-icons-50.png",
+  "page-icons/Math-icons-51.png",
+  "page-icons/Math-icons-52.png",
+  "page-icons/Math-icons-53.png",
+  "page-icons/Math-icons-54.png",
+  "page-icons/Math-icons-55.png",
+  "page-icons/Math-icons-56.png",
+  "page-icons/Math-icons-57.png",
+  "page-icons/Math-icons-58.png",
+  "page-icons/Math-icons-59.png",
+  "page-icons/Math-icons-60.png",
+  "page-icons/Math-icons-61.png",
+  "page-icons/Math-icons-62.png",
+  "page-icons/Math-icons-63.png",
+  "page-icons/Math-icons-64.png",
+  "page-icons/Math-icons-65.png",
+  "page-icons/Math-icons-66.png",
+  "page-icons/Math-icons-67.png",
+  "page-icons/Math-icons-68.png",
+  "page-icons/Math-icons-69.png",
+  "page-icons/Math-icons-70.png",
+  "page-icons/Math-icons-71.png",
+  "page-icons/Math-icons-72.png",
+  "page-icons/Math-icons-73.png",
+  "page-icons/Math-icons-74.png",
+  "page-icons/Math-icons-75.png",
+  "page-icons/Math-icons-76.png",
+  "page-icons/Math-icons-77.png",
+  "page-icons/Math-icons-78.png",
+  "page-icons/Math-icons-79.png",
+  "page-icons/Math-icons-80.png",
+  "page-icons/Math-icons-81.png",
+  "page-icons/Math-icons-82.png",
+  "page-icons/Math-icons-83.png",
+  "page-icons/Math-icons-84.png",
+  "page-icons/Math-icons-85.png",
+  "page-icons/Math-icons-86.png",
+  "page-icons/Math-icons-87.png",
+  "page-icons/Math-icons-88.png",
+  "page-icons/Math-icons-89.png",
+  "page-icons/Math-icons-90.png",
+  "page-icons/Math-icons-91.png",
+  "page-icons/Math-icons-92.png",
+  "page-icons/Math-icons-93.png",
+  "page-icons/Math-icons-94.png",
+  "page-icons/Math-icons-95.png",
+  "page-icons/Math-icons-96.png",
+  "page-icons/Math-icons-97.png",
+  "page-icons/Math-icons-98.png",
+  "page-icons/Science-icons_acids-bases.png",
+  "page-icons/Science-icons_alcohols-ethers-epoxides-sulfides.png",
+  "page-icons/Science-icons_aldehydes-ketones.png",
+  "page-icons/Science-icons_alkanes.png",
+  "page-icons/Science-icons_alkenes-alkynes.png",
+  "page-icons/Science-icons_amines.png",
+  "page-icons/Science-icons_aromatic-compounds.png",
+  "page-icons/Science-icons_atom.png",
+  "page-icons/Science-icons_atp.png",
+  "page-icons/Science-icons_biology.png",
+  "page-icons/Science-icons_brain.png",
+  "page-icons/Science-icons_carboxylic-acids.png",
+  "page-icons/Science-icons_cells.png",
+  "page-icons/Science-icons_cervical-spine.png",
+  "page-icons/Science-icons_chemical-reactions.png",
+  "page-icons/Science-icons_chemistry.png",
+  "page-icons/Science-icons_circulatory-system-diseases.png",
+  "page-icons/Science-icons_circulatory-system.png",
+  "page-icons/Science-icons_colon-disease.png",
+  "page-icons/Science-icons_conjugation-diels-alder-mo-theory.png",
+  "page-icons/Science-icons_cosmology-astronomy.png",
+  "page-icons/Science-icons_discoveries-projects.png",
+  "page-icons/Science-icons_dna.png",
+  "page-icons/Science-icons_electricity.png",
+  "page-icons/Science-icons_endocrine-system.png",
+  "page-icons/Science-icons_endocrinology-and-disabetes.png",
+  "page-icons/Science-icons_evolution.png",
+  "page-icons/Science-icons_fluids.png",
+  "page-icons/Science-icons_general-chemistry.png",
+  "page-icons/Science-icons_gravitation.png",
+  "page-icons/Science-icons_health-care-system.png",
+  "page-icons/Science-icons_health-miscellaneous.png",
+  "page-icons/Science-icons_health.png",
+  "page-icons/Science-icons_healthy-lifestyle.png",
+  "page-icons/Science-icons_hematologic-system.png",
+  "page-icons/Science-icons_human-biology.png",
+  "page-icons/Science-icons_ideal-gas-laws.png",
+  "page-icons/Science-icons_immune-system.png",
+  "page-icons/Science-icons_immunology.png",
+  "page-icons/Science-icons_lab-values.png",
+  "page-icons/Science-icons_linear-momentum.png",
+  "page-icons/Science-icons_mcat.png",
+  "page-icons/Science-icons_musculoskeletal-system.png",
+  "page-icons/Science-icons_nervous-system.png",
+  "page-icons/Science-icons_newtons-laws.png",
+  "page-icons/Science-icons_one-dimensional-motion.png",
+  "page-icons/Science-icons_orbitals-electrons.png",
+  "page-icons/Science-icons_organic-chemistry.png",
+  "page-icons/Science-icons_organic-structures.png",
+  "page-icons/Science-icons_oscillatory-motion.png",
+  "page-icons/Science-icons_oxidation-reduction.png",
+  "page-icons/Science-icons_periodic-table.png",
+  "page-icons/Science-icons_photosynthesis.png",
+  "page-icons/Science-icons_physics.png",
+  "page-icons/Science-icons_radioactive.png",
+  "page-icons/Science-icons_reaction-rates.png",
+  "page-icons/Science-icons_renal-system.png",
+  "page-icons/Science-icons_respiratory-system-diseases copy.png",
+  "page-icons/Science-icons_respiratory-system-diseases.png",
+  "page-icons/Science-icons_respiratory-system.png",
+  "page-icons/Science-icons_states-of-matter.png",
+  "page-icons/Science-icons_stereochemistry.png",
+  "page-icons/Science-icons_substitution-elimination-reactions.png",
+  "page-icons/Science-icons_thermodynamics.png",
+  "page-icons/Science-icons_torque.png",
+  "page-icons/Science-icons_tree-of-life.png",
+  "page-icons/Science-icons_two-dimensional-motion.png",
+  "page-icons/Science-icons_waves.png",
+  "page-icons/Science-icons_work-energy.png"
+];
+var pagePhotos = [
+  "page-photos/Science-icons_crash-course-biology.png",
+  "page-photos/Science-icons_discoveries.png",
+  "page-photos/Science-icons_discovery-lab-2012.png",
+  "page-photos/Science-icons_discovery-lab-2013.png",
+  "page-photos/Science-icons_earth-history.png",
+  "page-photos/Science-icons_lego-robotics.png",
+  "page-photos/Science-icons_life-on-earth.png",
+  "page-photos/Science-icons_projectile-launcher.png",
+  "page-photos/Science-icons_reverse-engineering.png",
+  "page-photos/Science-icons_robots.png",
+  "page-photos/Science-icons_simple-machines-explorations.png",
+  "page-photos/Science-icons_stellar-life.png",
+  "page-photos/Science-icons_thermo-can.png",
+  "page-photos/Science-icons_universe-scale.png"
+];
+
+var ScreenStyle = {
+  A: 0,
+  B: 1
+};
+
+var randomNumber = function(min, max) {
+  if (min > max) {
+    var temp = min;
+    min = max;
+    max = temp;
+  }
+  var a = min;
+  var b = max - min;
+  return a + (Math.random() * b);
+};
+
+var randomInteger = function(min, max) {
+  return Math.round(randomNumber(min, max));
+};
+
+var getRandomElementInArray = function(a) {
+  var i = randomInteger(0, a.length - 1);
+  return a[i];
+}
+
+var getRandomPageTitle = function() {
+  return getRandomElementInArray(pageTitles);
+};
+
+var getRandomPageImageFilename = function(isIcon) {
+  var a = isIcon ? pageIcons : pagePhotos;
+  return getRandomElementInArray(a);
+};
+
+$(document).ready(function() {
+  var AndroidStatusBar = React.createClass({displayName: "AndroidStatusBar",
+    render: function() {
+      return React.createElement("div", {
+        className: "android-status-bar", 
+        style: {
+          width: "100%",
+          height: "25px",
+          backgroundImage: "url(images/fake/android-status-bar-320x25.png)",
+          backgroundColor: "#000"
+        }
+      });
+    }
+  });
+  
+  var AndroidNavBar = React.createClass({displayName: "AndroidNavBar",
+    render: function() {
+      return React.createElement("div", {
+        className: "android-nav-bar", 
+        style: {
+          width: "100%",
+          height: "48px",
+          backgroundImage: "url(images/fake/android-nav-bar-320x48.png)",
+          backgroundColor: "#000"
+        }
+      });
+    }
+  });
+  
+  var PageHeader = React.createClass({displayName: "PageHeader",
+    render: function() {
+      var styleID = this.props.styleID;
+      var imageFilename = this.props.imageFilename;
+      
+      var title = getRandomPageTitle();
+      var h1 = React.createElement("h1", {
+        className: "title-2",
+        style: {
+          position: "absolute",
+          left: "16px",
+          bottom: "16px",
+          margin: "0",
+          padding: "0",
+          color: "#fff",
+          width: "256px"
+        }
+      }, title);
+      
+      var isIcon = imageFilename.match(/^page-icons/);
+      var gradientColor = "0, 0, 0";
+      if (isIcon) {
+        if (imageFilename.match(/Math-/)) {
+          gradientColor = "38, 109, 124";
+        } else {
+          gradientColor = "132, 63, 72";
+        }
+      }
+      var imageURL = "images/" + imageFilename;
+      
+      var gradient = React.createElement("div", {
+        style: {
+          position: "absolute",
+          left: "0",
+          top: "0",
+          width: "100%",
+          height: "100%",
+          background: "-webkit-linear-gradient(top, rgba(" + gradientColor + ", 0) 0%, rgba(" + gradientColor + ", 0.32) 50%, rgba(" + gradientColor + ", 0.64) 100%)"
+        }
+      });
+      var image = null;
+      if (styleID == ScreenStyle.A) {
+        image = React.createElement("div", {
+          style: {
+            position: "absolute",
+            left: "96px",
+            top: "26px",
+            width: "128px",
+            height: "128px",
+            borderRadius: "90px",
+            background: "url(" + imageURL + ") no-repeat center",
+            backgroundSize: "contain"
+          }
+        }); 
+      } else {
+        image = React.createElement("div", {
+          style: {
+            position: "absolute",
+            left: "0",
+            top: "0",
+            width: "100%",
+            height: "100%",
+            background: "url(" + imageURL + ") no-repeat center",
+            backgroundSize: "cover"
+          }
+        }); 
+      }
+      
+      return React.createElement("div", {
+        className: "page-header", 
+        style: {
+          position: "absolute",
+          left: "0",
+          top: "0",
+          width: "320px",
+          height: "180px",
+          backgroundColor: "rgb(" + gradientColor + ")"
+        }
+      }, image, gradient, h1);
+    }
+  });
+  
+  var App = React.createClass({displayName: "App",
+    render: function() {
+      var pageHeader = React.createElement(PageHeader, {
+        styleID: i, 
+        imageFilename: imageFilename
+      });
+      return React.createElement("div", {
+        className: "app", 
+        style: {
+          position: "relative",
+          left: "0",
+          top: "0",
+          width: "320px",
+          height: "455px"
+        }
+      }, pageHeader);
+    }
+  }); 
+  
+  var Canvas = React.createClass({displayName: "Canvas", 
+    render: function() {
+      var statusBar = React.createElement(AndroidStatusBar);
+      var app = React.createElement(App, {
+        styleID: i, 
+        imageFilename: imageFilename
+      });
+      var navBar = React.createElement(AndroidNavBar);
+      return React.createElement("div", null, statusBar, app, navBar);
+    }
+  });
+  
+  // Get the filename for the image we're using:
+  var iconFilename = getRandomPageImageFilename(true);
+  var photoFilename = getRandomPageImageFilename(false);
+  
+  // Create some screens:
+  var numScreens = 3;
+  for (var i = 0; i < numScreens; i++) {
+    var screenID = "screen-" + (i + 1);
+    $("body").append("<div id=\"" + screenID + "\" class=\"screen\"></div>");
+    var container = $("#" + screenID).get(0);
+    var imageFilename = (i < 2) ? iconFilename : photoFilename;
+    React.render(React.createElement(Canvas, {
+      styleID: i, 
+      imageFilename: imageFilename
+    }), container);
+  }
+});
